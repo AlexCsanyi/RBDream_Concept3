a43912f1bb48497480df248d277a7091c2a64919
AlexCsanyi <alex.csanyi@gmail.com>
2019-10-30 06:41:53 +0000

    added internal Links to pages

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,6 +5,16 @@ const ContactPage = () => {
     <div>
       <h1>Contact RB Dream</h1>
       <p>email: info@rbdream.com</p>
+      <p>
+        You can visit our facebook page:
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.facebook.com/radoslavbarbulrale/"
+        >
+          @radoslavbarbulrale
+        </a>
+      </p>
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,14 @@
 import React from "react"
+import { Link } from "gatsby"
 
 const IndexPage = () => {
   return (
     <div>
       <h1>Hello!</h1>
       <h2>This is RB Dream, a fishing store in beautiful Kikinda</h2>
+      <p>
+        Want to get in touch? <Link to="/contact">Contact us</Link>
+      </p>
     </div>
   )
 }
