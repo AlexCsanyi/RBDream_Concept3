8998217a253cdc485bf8d475e73a0cd8382e8546
AlexCsanyi <alex.csanyi@gmail.com>
2019-10-30 06:11:26 +0000

    built 4 main components and exported them

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>RB Dream - Kikinda</h1>
+      <p>Our shop's story is going to be amazing!</p>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const ContactPage = () => {
+  return (
+    <div>
+      <h1>Contact RB Dream</h1>
+      <p>email: info@rbdream.com</p>
+    </div>
+  )
+}
+
+export default ContactPage
