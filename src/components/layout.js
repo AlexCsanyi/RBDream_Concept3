11c4bf206dd599cc933273e7e678686c1986d49e
AlexCsanyi <alex.csanyi@gmail.com>
2019-10-31 07:01:22 +0000

    created 2 key scss modules

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,10 +3,11 @@ import React from "react"
 import Footer from "./footer"
 import Header from "./header"
 import "../styles/index.scss"
+import layoutStyles from "./layout.module.scss"
 
 const Layout = props => {
   return (
-    <div>
+    <div className={layoutStyles.container}>
       <Header />
       {props.children}
       <Footer />
