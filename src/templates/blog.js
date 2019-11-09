0fcb12aa960f86bfc1296ea1d96c791d655cc965
AlexCsanyi <alex.csanyi@gmail.com>
2019-11-09 18:18:25 +0000

    started working on rendering the body

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
 import { graphql } from "gatsby"
+import {documentToReactComponents} from '@contentful/rich-text-react-renderer'
 
 /*
 
@@ -19,8 +20,37 @@ export const query = graphql`
 `
 */
 
+export const query = graphql`
+  query($slug: String!) {
+    contentfulBlogPost(slug: {eq: $slug}) {
+      title
+      publishedDate(formatString: "Do MMM YYYY")
+      body {
+        json
+      }
+    }
+  }
+`
+
 const Blog = props => {
-  return <Layout></Layout>
+  const options = {
+    renderNode: {
+      "embedded-asset-block": (node) => {
+        const alt = node.data.target.fields.title['en-US']
+        const url = node.data.target.fields.file['en-US'].url
+        
+        return <img alt={alt} src={url} />
+      }
+    }
+  }
+
+  return (
+  <Layout>
+    <h1>{props.data.contentfulBlogPost.title}</h1>
+    <p>{props.data.contentfulBlogPost.publishedDate}</p>
+    {documentToReactComponents(props.data.contentfulBlogPost.body.json, options)}
+  </Layout>
+  )
 }
 
 export default Blog
