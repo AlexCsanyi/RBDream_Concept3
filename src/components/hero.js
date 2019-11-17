9f1e78927d5b5e7d9a64182c7c24019d2eed2ec8
AlexCsanyi <alex.csanyi@gmail.com>
2019-11-17 06:58:46 +0000

    small adjustment to improve UI

--- src/components/hero.js
+++ src/components/hero.js
@@ -30,7 +30,7 @@ const Hero = () => {
         <Button></Button>
         <svg
           width="100%"
-          height="150"
+          height="100"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
         >
